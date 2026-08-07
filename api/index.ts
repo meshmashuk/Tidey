3befbd201cec98ec86a30f312fabcee66b50427a
davidtodman <david.todman@meshmash.co.uk>
2026-08-07 12:19:29 +0100

    Updated api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,13 @@
-import app from "../server/dist/index.js";
+import type { IncomingMessage, ServerResponse } from "http";
 
-export default app;
+// Cache the dynamic import across invocations (warm starts) so we're not
+// re-importing on every request.
+let appPromise: Promise<any> | null = null;
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+  if (!appPromise) {
+    appPromise = import("../server/dist/index.js").then((m) => m.default);
+  }
+  const app = await appPromise;
+  return app(req, res);
+}
